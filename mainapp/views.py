e768b949bc81e62b67f580cd01ba4f226a8b266e
slade <paran87@mail.ru>
2022-10-30 20:35:51 -0300

    task 5

--- mainapp/views.py
+++ mainapp/views.py
@@ -6,6 +6,30 @@ from django.views.generic import TemplateView
 class ContactsView(TemplateView):
     template_name = 'mainapp/contacts.html'
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['contacts'] = [
+            {
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHcrhA',
+                'city': 'Санкт‑Петербург',
+                'phone': '+7-999-11-11111',
+                'email': 'geeklab@spb.ru',
+                'address': 'территория Петропавловская крепость, 3Ж',
+            }, {
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHX3xB',
+                'city': 'Казань',
+                'phone': '+7-999-22-22222<',
+                'email': 'geeklab@kz.ru',
+                'address': 'территория Кремль, 11, Казань, Республика Татарстан, Россия',
+            }, {
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHh9kD',
+                'city': 'Москва',
+                'phone': '+7-999-33-33333',
+                'email': 'geeklab@msk.ru',
+                'address': 'Красная площадь, 7, Москва, Россия',
+            },
+        ]
+        return context_data
 
 class CoursesView(TemplateView):
     template_name = 'mainapp/courses_list.html'
