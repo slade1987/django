46217cfef3580a461034acd3074c1ab80922649f
slade <paran87@mail.ru>
2022-12-13 01:25:26 +0300

    Lesson 6 Admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
+from mainapp.models import News, Course, Lesson, CourseTeacher
 
-from mainapp.models import News
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(CourseTeacher)
 
-admin.site.register(News)
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'deleted', 'slug')
+    list_filter = ('deleted',)
+    search_fields = ('title',)
+    actions = ('mark_as_delete',)
+
+    # Вовод читаемой новости
+    def slug(self, obj):
+        return obj.title.lower().replace(' ', '-')
+
+    def mark_as_delete(self, request, queryset):
+        queryset.update(deleted=True)
+
+    mark_as_delete.short_description = 'Пометить удаленным '
